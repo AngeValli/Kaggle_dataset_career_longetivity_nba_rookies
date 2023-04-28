3e501590304f9727d4d9ee979ec35a75cae88cdd
AngeValli <ange.valli@free.fr>
2023-04-28 18:51:34 +0200

    add clustering and features engineering  Modifications qui seront validées : 	modifié :         API.py 	modifié :         Notebook.ipynb 	modifié :         __pycache__/constants.cpython-310.pyc 	modifié :         constants.py 	modifié :         imputer.pickle 	nouveau fichier : kmeans_model.pickle 	modifié :         model_lgrg.pickle 	modifié :         model_rf.pickle 	modifié :         model_svc.pickle 	modifié :         normalizer.pickle

--- API.py
+++ API.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from typing import List
 import pandas as pd
 import numpy as np
 import pickle as pick
@@ -15,13 +16,19 @@ def predict():
     :param: None
     :return: The prediction in JSON format
     """
-    raw_data: pd.DataFrame = request.get_json()
+    raw_data: List[float] = request.get_json()
+
+    # verify data inputs
+    assert all((isinstance(x, float) for x in l) for l in raw_data)
 
     # transformations
     data: np.array = imputer.transform(raw_data)
+    preds: np.array = k_means_labeling.predict(data)
+    preds = preds.reshape(len(preds), 1)
+    data = np.append(data, preds, axis=1)
     data = normalizer.transform(data)
 
-    #predict using majority voting between the three models
+    # predict using majority voting between the three models
     votes = model_rf.predict(data) + model_svc.predict(data) + model_lgrg.predict(data)
     prediction = np.array2string(1*(votes>1))
 
@@ -29,6 +36,7 @@ def predict():
 
 if __name__ == '__main__':
     imputer: pick = pick.load(open(c.IMPUTER, "rb"))
+    k_means_labeling: pick = pick.load(open(c.KMEANS_MODEL, "rb"))
     model_rf: pick = pick.load(open(c.MODEL_RF,"rb"))
     model_svc: pick = pick.load(open(c.MODEL_SVC,"rb"))
     model_lgrg: pick = pick.load(open(c.MODEL_LGRG,"rb"))
